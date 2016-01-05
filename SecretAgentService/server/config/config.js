d09143edf316993bda4cc88f76e6e9477c388309
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-05 14:10:00 +0200

    Change server port

--- SecretAgentService/server/config/config.js
+++ SecretAgentService/server/config/config.js
@@ -5,11 +5,11 @@ module.exports = {
     development: {
         rootPath: rootPath,
         db: 'mongodb://localhost/telerikacademycourses',
-        port: process.env.PORT || 3030
+        port: process.env.PORT || 3000
     },
     production: {
         rootPath: rootPath,
         db: 'mongodb://admin:dsadsadsadsadsadsadsaewerwewtewfdfsgfsdfdsfefdsgfdhtrytett@ds027328.mongolab.com:27328/telerikacademycourses',
-        port: process.env.PORT || 3030
+        port: process.env.PORT || 3000
     }
 };
