4eaa145eef5cf27d7dfd66c18e89ad3f48ed05ba
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-10 16:11:03 +0200

    Add folder lib

---
+++ SecretAgentService/public/lib/page-animation/dist/js/custom-animation.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".animsition").animsition({
+        inClass: 'flip-in-y-fr',
+        outClass: 'flip-out-y-fr',
+        inDuration: 1500,
+        outDuration: 800,
+        linkElement: '.animsition-link',
+        // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+        loading: true,
+        loadingParentElement: 'body', //animsition wrapper element
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: [ 'animation-duration', '-webkit-animation-duration'],
+        overlay : false,
+        overlayClass : 'animsition-overlay-slide',
+        overlayParentElement : 'body',
+        transition: function(url){ window.location.href = url; }
+    });
+});
