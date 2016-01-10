f6aae914a7453cfa6ecf489d969b2e8402fb4f4c
Chavdar Angelov <c.l.angelov@gmail.com>
2016-01-10 15:23:26 +0200

    admin is ready and kickin'

--- SecretAgentService/public/app/listUsers/ListUsersController.js
+++ SecretAgentService/public/app/listUsers/ListUsersController.js
@@ -7,11 +7,12 @@
 
         vm.deleteUser = function(event) {
             var userId = event.target.getAttribute('data-value');
+            var adress = event.target.getAttribute('data-adress');
             console.log(event);
             listUsersData.deleteUser(userId)
                 .then(function (data) {
                     notifier.success('You have deleted this user successfully');
-                    window.location.href = "/";
+                    window.location.href = "/users/" + adress;
                 })
         };
     }
