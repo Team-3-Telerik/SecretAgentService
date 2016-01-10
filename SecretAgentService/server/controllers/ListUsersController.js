2b6ffbfa6523535794d464c22c2b217f0608474c
Nikolai <nikolaimishev@gmail.com>
2016-01-10 15:23:47 +0200

    Revert "added paging on agents and commissioners pages"
    This reverts commit 84ffdcc0ce5f496d4815852c81e17970c8de3115.

--- SecretAgentService/server/controllers/ListUsersController.js
+++ SecretAgentService/server/controllers/ListUsersController.js
@@ -1,84 +1,37 @@
 var User = require('mongoose').model('User'),
     Mission = require('mongoose').model('Mission'),
-    clientPages;
-
+    DEFAULT_PAGE_SIZE = 10,
+    DEFAULT_PAGE = 1;
 
 module.exports = {
     getAllAgents: function (req, res, next) {
-        var NUMBER_OF_ITEMS = 10,
-            page = req.query.page || 1;
+
 
         User.find({roles: ['Agent']})
-            .skip(NUMBER_OF_ITEMS * (page - 1))
-            .limit(NUMBER_OF_ITEMS)
+            .skip((req.query.page || DEFAULT_PAGE) - 1)
+            .limit(DEFAULT_PAGE_SIZE)
             .exec(function (err, agents) {
                 if (err) {
                     console.log('Get all users failed: ' + err);
                     return;
                 }
-                    console.log(agents);
-                if (!clientPages) {
 
-                    User
-                        .find({roles: ['Agent']})
-                        .exec(function (err, users) {
-                            clientPages = Math.ceil(users.length / 10);
-
-                            res.render('../views/users/agents', {
-                                agents: agents,
-                                currentUser: req.user,
-                                clientPages: clientPages
-                            });
-                            console.log('paging users success!');
-                        })
-                }
-                else {
-                    res.render('../views/users/agents', {
-                        agents: agents,
-                        currentUser: req.user,
-                        clientPages: clientPages
-                    });
-
-                }
+                res.render('../views/users/agents', {agents: agents, currentUser: req.user});
             })
     },
 
     getAllCommissioners: function (req, res, next) {
-        var NUMBER_OF_ITEMS = 10,
-            page = req.query.page || 1;
-
         User
             .find({roles: ['Commissioner']})
-            .skip(NUMBER_OF_ITEMS * (page - 1))
-            .limit(NUMBER_OF_ITEMS)
+            .skip((req.query.page || DEFAULT_PAGE) - 1)
+            .limit(DEFAULT_PAGE_SIZE)
             .exec(function (err, commissioners) {
                 if (err) {
                     console.log('Get all users failed: ' + err);
                     return;
                 }
-                if (!clientPages) {
 
-                    User
-                        .find({roles: ['Commissioner']})
-                        .exec(function (err, users) {
-                            clientPages = Math.ceil(users.length / 10);
-
-                            res.render('../views/users/commissioners', {
-                                commissioners: commissioners,
-                                currentUser: req.user,
-                                clientPages: clientPages
-                            });
-                            console.log('paging commissioners success!');
-                        })
-                }
-                else {
-                    res.render('../views/users/commissioners', {
-                        commissioners: commissioners,
-                        currentUser: req.user,
-                        clientPages: clientPages
-                    });
-
-                }
+                res.render('../views/users/commissioners', {commissioners: commissioners, currentUser: req.user});
             })
     }
 };
