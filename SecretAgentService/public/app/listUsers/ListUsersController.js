6905abd814883f7b922e84cc8397c597494c835f
Chavdar Angelov <c.l.angelov@gmail.com>
2016-01-10 14:58:48 +0200

    user controll added

--- SecretAgentService/public/app/listUsers/ListUsersController.js
+++ SecretAgentService/public/app/listUsers/ListUsersController.js
@@ -5,8 +5,16 @@
         var vm = this;
         vm.identity = identity;
 
+        vm.deleteUser = function(event) {
+            var userId = event.target.getAttribute('data-value');
+            console.log(event);
+            listUsersData.deleteUser(userId)
+                .then(function (data) {
+                    notifier.success('You have deleted this user successfully');
+                    window.location.href = "/";
+                })
+        };
     }
-
 
     angular.module('app.controllers')
         .controller('ListUsersController', ['identity', 'listUsersData', 'notifier', ListUsersController]);
--- SecretAgentService/server/config/routes.js
+++ SecretAgentService/server/config/routes.js
@@ -24,6 +24,7 @@ module.exports = function(app) {
 
     app.get('/admin', auth.isInRole('admin'),controllers.admin.getAdminPanel);
     app.delete('/missions/:id', auth.isInRole('admin'), controllers.mission.deleteMission);
+    app.delete('/users/:id', auth.isInRole('admin'), controllers.users.deleteUser);
 
     app.get('/users/agents', controllers.listUsers.getAllAgents);
     app.get('/users/commissioners', controllers.listUsers.getAllCommissioners);
