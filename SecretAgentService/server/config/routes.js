29668c274a29840229752cd8ca7fdfa191caf5e7
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-13 01:27:58 +0200

    Added admin kendo grid for user management

--- SecretAgentService/server/config/routes.js
+++ SecretAgentService/server/config/routes.js
@@ -11,7 +11,8 @@ module.exports = function(app) {
     app.get('/api/users', auth.isInRole('admin'), controllers.users.getAllUsers);
     app.post('/api/users', controllers.users.createUser);
     app.put('/api/users', auth.isAuthenticated, controllers.users.updateUser);
-
+    app.post('/api/users/delete', auth.isInRole('admin'), controllers.users.deleteUser);
+    app.post('/api/users/update', auth.isInRole('admin'), controllers.users.updateUser);
     app.get('/api/statistics', controllers.statistics.getStatistics);
 
     app.get('/profile/edit', auth.isAuthenticated, function (req, res) {
