9f64815d88f3f27bd9bee20b7a2923410f38d8ef
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-13 13:10:43 +0200

    Fix bug in chart statistics

--- SecretAgentService/public/app/common/polar-chart.js
+++ SecretAgentService/public/app/common/polar-chart.js
@@ -1,53 +1,50 @@
-(function () {
-    $.ajax({
-            url: 'http://localhost:3000/api/statistics'
-        })
-        .then(function (statistics) {
+$.ajax({
+        url: 'http://localhost:3000/api/statistics'
+    })
+    .then(function (statistics) {
 
-            var data = [
-                {
-                    value: statistics.commissioners,
-                    color: "#F7464A",
-                    highlight: "#FF5A5E",
-                    label: "Commissioners"
-                },
-                {
-                    value: statistics.agents,
-                    color: "#46BFBD",
-                    highlight: "#5AD3D1",
-                    label: "Agents"
-                },
-                {
-                    value: statistics.missions,
-                    color: "#9966ff",
-                    highlight: "#9933cc",
-                    label: "Missions"
-                }
-            ];
+        var data = [
+            {
+                value: statistics.commissioners,
+                color: "#F7464A",
+                highlight: "#FF5A5E",
+                label: "Commissioners"
+            },
+            {
+                value: statistics.agents,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Agents"
+            },
+            {
+                value: statistics.missions,
+                color: "#9966ff",
+                highlight: "#9933cc",
+                label: "Missions"
+            }
+        ];
 
+        $( document ).ready(function() {
             if (document.getElementById("chart-area")) {
-
                 var ctx = document.getElementById("chart-area").getContext("2d");
                 window.myPolarArea = new Chart(ctx).Pie(data, {
                     responsive: true
                 });
-
             }
         });
-
-    $('#chart-area').click(function (evt) {
-        var activePoints = window.myPolarArea.getSegmentsAtEvent(evt);
-        if (activePoints[0]) {
-            if(activePoints[0].label == 'Missions'){
-                window.location.href = "/missions";
-            }
-            if(activePoints[0].label == 'Commissioners'){
-                window.location.href = "/users/commissioners";
-            }
-            if(activePoints[0].label == 'Agents'){
-                window.location.href = "/users/agents";
-            }
-        }
     });
 
-}());
+$('#chart-area').click(function (evt) {
+    var activePoints = window.myPolarArea.getSegmentsAtEvent(evt);
+    if (activePoints[0]) {
+        if (activePoints[0].label == 'Missions') {
+            window.location.href = "/missions";
+        }
+        if (activePoints[0].label == 'Commissioners') {
+            window.location.href = "//users/agents";
+        }
+        if (activePoints[0].label == 'Agents') {
+            window.location.href = "/missions";
+        }
+    }
+});
